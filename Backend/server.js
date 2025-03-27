e2d4f0f550a58bc566a0e1d173e2400dbb53274f
BS-eng2023 <azaam.mahmood@mail.de>
2025-03-27 12:10:18 +0100

    start project

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,11 @@
-let username ="Azaam"
-console.log("username: "+username);
-const { time } = require('node:console');
-const http= require('node:http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-function 
-handelRequest(request,response){
-    request.server="mypage";
-    response.statusCode=200;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-    if (request.url==="/currenttime"){
-
-        response.end('<h1>'+ new Date().toISOString()+'</h1>');
-
-    } else if (request.url==="/"){
-
-        response.end('<h1>Hello Azaam</h1>');
-
-    }
-
-
-}
-const server =http.createServer (handelRequest);
-server.listen(3000);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
